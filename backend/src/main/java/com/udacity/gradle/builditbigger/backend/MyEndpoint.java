bf804c796a5cb49e5cd2ae8c55bbcc96e4514a32
Kelli <keldavis5@yahoo.com>
2018-12-23 02:21:18 -0500

    update backend

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -4,7 +4,7 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
-import javax.inject.Named;
+import com.kelldavis.builditbigger.jokelibrary.Joke;
 
 /** An endpoint class we are exposing */
 @Api(
@@ -18,11 +18,11 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    /** A simple endpoint method that returns a joke response */
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData(Joke.getJoke());
 
         return response;
     }
